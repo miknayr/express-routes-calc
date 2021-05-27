86d13b44a069700c5a5e4dc44c183bef5c957051
miknayr <teamry4n@gmail.com>
2021-05-27 15:44:49 -0700

    bonus from george

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ app.get ('/', (req, res) => {
     
 })
 
-/* get requests -- query strongs */
-
-
-/* post - req body -- redirects */
-
-
-/* ALL REQUESTS: REQ PARAMETERS */
-
 app.get('/add/:x/:y', (req, res) =>{
     const x = parseInt(req.params.x)
     const y = parseInt(req.params.y)
@@ -48,6 +40,30 @@ app.get('/divide/:x/:y', (req, res) =>{
 
 })
 
+
+
+
+
+app.get("/*", function(req, res) {
+    let math=req.query.math
+    let nums = req.params[0].split('/')
+    nums = nums.map(num => parseInt(num))
+    switch(math) {
+        case 'add':
+            res.send(`${nums.reduce((a,b) => a + b)}`)
+            break
+        case 'subtract':
+            res.send(`${nums.reduce((a,b) => a - b)}`)
+            break
+        case 'multiply':
+            res.send(`${nums.reduce((a,b) => a * b)}`)
+            break
+        case 'divide':
+            res.send(`${nums.reduce((a,b) => a / b)}`)
+            break
+    }
+
+  });
 
 app.listen(PORT, () => {
     console.log(`port ${PORT} is being used`)
